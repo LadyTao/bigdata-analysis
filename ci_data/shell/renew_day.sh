79e4def7b6c2400bc8337ff1ee3ee7268e799472
zhaopeng <zhaopeng@wondershare.cn>
2019-01-15 20:02:03 +0800

    add the renew_day.sh in the shell folder

---
+++ ci_data/shell/renew_day.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+first_date="2019-01-11"
+expire=`date +"%Y-%m-%d"`
+end_date=`date -d "8 days"  +"%Y-%m-%d"`
+while [[ $first_date < $end_date  ]]
+do
+        echo $first_date
+		hive -hiveconf expire=$first_date -f   /usr/local/bigdata/jobtaskh0/shelljob/ci_data/ci_renew_day.sql
+        first_date=`date -d "+1 day $first_date"  +"%Y-%m-%d"`
+done
