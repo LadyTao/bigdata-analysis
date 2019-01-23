db65e839caab7d0284f75d72045cce476a19cea5
zhaopeng <zhaopeng@wondershare.cn>
2019-01-23 16:09:57 +0800

    modify the  date

--- ci_data/shell/import_expire_member_everyday.sh
+++ ci_data/shell/import_expire_member_everyday.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-expire_date=`date  +'%Y-%m-%d'`
-#expire_date=`date -d "1 days"  +"%Y-%m-%d"`
+#expire_date=`date  +'%Y-%m-%d'`
+expire_date=`date -d "14 days"  +"%Y-%m-%d"`
 
 
 hive<<EOF
 
-use dbsync;insert into dbsync.ci_wx_member_expire_date partition(expireDate='$expire_date') select id,uid,vip_type,endtime,status,base_info, mobile,inputtime,updatetime,pay_mode from dbsync.shencut_store_wx_member where from_unixtime(endtime+28800,'yyyy-MM-dd')='$expire_date';
+use dbsync;insert into dbsync.ci_wx_member_expire_date partition(expireDate='$expire_date') select id,uid,vip_type,endtime,status,base_info, mobile,inputtime,updatetime,pay_mode from dbsync.shencut_store_wx_member ;
 
 EOF
