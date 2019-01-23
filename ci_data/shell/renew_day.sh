a65444bcae189dfbbae70fb826224f94dd84d6db
zhaopeng <zhaopeng@wondershare.cn>
2019-01-23 16:10:36 +0800

    modify the  date

--- ci_data/shell/renew_day.sh
+++ ci_data/shell/renew_day.sh
@@ -3,9 +3,8 @@
 # calculate the expire and renew users info  and insert the result to mart.ci_member_renew_rate_day in hive;
 
 first_date="2019-01-11"
-expire=`date +"%Y-%m-%d"`
-end_date=`date -d "8 days"  +"%Y-%m-%d"`
-while [[ $first_date < $end_date  ]]
+expire=`date  +"%Y-%m-%d"`
+while [[ $first_date < $expire ]]
 do
         echo $first_date
 		hive -hiveconf expire=$first_date -f   /usr/local/bigdata/jobtaskh0/shelljob/ci_data/ci_renew_day.sql
