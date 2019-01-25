a0bfabab438479fea695fbba37307378306ea63d
zhaopeng <zhaopeng@wondershare.cn>
2019-01-25 10:31:09 +0800

    modify the sqoop shell of wx_order in order to import all the data to hive everyday

--- ci_data/shell/import_ci_hive.sh
+++ ci_data/shell/import_ci_hive.sh
@@ -5,11 +5,18 @@
 #echo 'append the wx_member in store db'
 #sqoop job --exec  shencut_store_wx_member_add
 echo 'append the wx_order in store db'
-sqoop job --exec  shencut_store_wx_order_add
+#sqoop job --exec  shencut_store_wx_order_add
 echo 'append the wx_member_recharge_record in store db'
 sqoop job --exec  shencut_store_wx_member_recharge_record_add
 
 
+
+
+echo "beigin to delete the data  in hive:dbsync.shencut_store_wx_member"
+hadoop fs -rm -r hdfs://hdp-0.local:8020/path/dbsync/shencut_store_wx_order
+# tinyint to string
+sqoop import --connect jdbc:mysql://192.168.11.43:3306/store?tinyInt1isBit=false --username vp_store_read --password ppYDC##821348  -m 4  --hive-overwrite  --table wx_order  --map-column-hive type=STRING,from=STRING,pay_origin=STRING,status=STRING,pay_type=STRING,open_vip_type=STRING   --hive-database dbsync --hive-table  shencut_store_wx_order  --target-dir  'hdfs://hdp-0.local:8020/path/dbsync/shencut_store_wx_order' --hive-drop-import-delims --fields-terminated-by '\001'  --lines-terminated-by '\n'
+echo "load  wx_order data finished!! "
 
 echo "beigin to delete the data  in hive:dbsync.shencut_store_wx_member"
 hadoop fs -rm -r hdfs://hdp-0.local:8020/path/dbsync/shencut_store_wx_member
