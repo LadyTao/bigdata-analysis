181811896fb4e9e9dc1abd9abfe805a85a599b03
zhaopeng <zhaopeng@wondershare.cn>
2019-04-01 19:00:43 +0800

    [feature] 添加相关的airflow 任务模板

--- ci_data/pyspark_template/pyspark_template.py
+++ ci_data/pyspark_template/pyspark_template.py
@@ -9,6 +9,9 @@ from pprintpp import pprint as pp
 import pyspark.sql.functions as F
 
 import datetime
+import calendar
+import argparse
+from dateutil.parser import parse
 
 """
 spark-submit pyspark  file with special resources demo
@@ -16,10 +19,18 @@ spark-submit pyspark  file with special resources demo
 ./bin/spark-submit    --num-executors 4 --executor-memory 4G --executor-cores 4 --driver-memory 4G --driver-cores 4 path/somefile.py
 
 
- 插入mysql 时一定要添加  --jars  --driver-class-path  
-./bin/spark-submit  --num-executors 4 --executor-memory 4G --executor-cores 4 --driver-memory 4G --driver-cores 4 --jars /usr/hdp/3.0.1.0-187/spark2/jars/mysql-connector-java-5.1.47.jar --driver-class-path /usr/hdp/3.0.1.0-187/spark2/jars/mysql-connector-java-5.1.47.jar  /usr/local/bigdata/jobtaskh0/pythonjob/uos/uid_label/test.py day '2019-03-28' 
-
-
+ 插入mysql 时一定要添加  --jars  --driver-class-path  任务时间类型参数：day,week,month 
+./bin/spark-submit \
+--num-executors 4 \
+--executor-memory 4G \
+--executor-cores 4 \
+--driver-memory 4G \
+--driver-cores 4 \
+--jars /usr/hdp/3.0.1.0-187/spark2/jars/mysql-connector-java-5.1.47.jar \
+ --driver-class-path /usr/hdp/3.0.1.0-187/spark2/jars/mysql-connector-java-5.1.47.jar \
+/usr/local/bigdata/jobtaskh0/pythonjob/pyspark_template/pyspark_template.py \
+day \
+'2019-03-28' 
 
 """
 
@@ -67,8 +78,6 @@ def insert_to_hive(sql_query, sql_session):
     print("begin to insert data to hive")
     df = sql_session.sql(sql_query)
     print("insert into hive table done")
-
-
 
 
 def day(date, moudle_sql):
@@ -127,7 +136,85 @@ def month(date, moudle_sql):
     return moudle_sql
 
 
+def job_day(date, moudle_sql):
+    """
+     利用调度日期参数，替换model_sql的日期参数，替换后的module_sql作为执行sql,传入相关的sparksession,执行相关的日任务
+    :param date:job_day: '2019-03-30' 一般为每天，获取相关的执行周期数据，day类型的任务一般为：该天
+    :param moudle_sql:
+    :return: model_sql
+    Examples
+    --------
+    usage:
 
+    # >>> module_sql= ' select * from  db.table where day between __DAY1__ and __DAY2__ '
+    # >>> jod_date = '2019-03-25'
+    # >>> excute_sql = job_day(date= job_date,module_sql= module_sql)
+    # >>> print("excute_sql:",excute_sql)
+    "excute_sql":  " select * from  db.table where day between '2019-03-25' and '2019-03-25' "
+
+    """
+
+    date = date.strftime('%Y-%m-%d')
+
+    moudle_sql = moudle_sql.replace('__DAY1__', str(date)).replace('__DAY2__',
+                                                                   str(date))
+
+    return moudle_sql
+
+
+def job_week(date, moudle_sql):
+    """
+    利用调度日期参数，替换model_sql的日期参数，替换后的module_sql作为执行sql,传入相关的sparksession,执行相关的周任务
+    :param date: job_week ：'2019-03-25' 一般为每周的周一，获取相关的执行周期数据，week类型的任务一般为：周一到该周的周日
+    :param moudle_sql:
+    :return:modeule_sql:excute_sql
+
+     Examples
+    --------
+    usage:
+
+    # >>> module_sql= ' select * from  db.table where day between __DAY1__ and __DAY2__ '
+    # >>> jod_date = '2019-03-25'
+    # >>> excute_sql = job_day(date= job_date,module_sql= module_sql)
+    # >>> print("excute_sql:",excute_sql)
+    "excute_sql":  " select * from  db.table where day between '2019-03-25' and '2019-03-31' "
+    """
+
+    date_start = date.strftime('%Y-%m-%d')
+
+    date_end = (date + datetime.timedelta(days=6)).strftime('%Y-%m-%d')
+
+    moudle_sql = moudle_sql.replace('__DAY1__', str(date_start)).replace(
+        '__DAY2__', str(date_end))
+    return moudle_sql
+
+
+def job_month(date, moudle_sql):
+    """
+    利用调度日期参数，替换model_sql的日期参数，替换后的module_sql作为执行sql,传入相关的sparksession,执行相关的月任务
+    :param date: job_date:'2019-03-01' 一般为每月的第一天，month类型的任务一般为该月的第一天到最后一天。
+    :param moudle_sql:moudle_sql
+    :return:
+
+    Examples
+    --------
+    usage:
+
+    # >>> module_sql= ' select * from  db.table where day between __DAY1__ and __DAY2__ '
+    # >>> jod_date = '2019-03-25'
+    # >>> excute_sql = job_day(date= job_date,module_sql= module_sql)
+    # >>> print("excute_sql:",excute_sql)
+    "excute_sql":  " select * from  db.table where day between '2019-03-01' and '2019-03-31' "
+    """
+
+    date_start = date.strftime('%Y-%m-%d')
+    monthrange = calendar.monthrange(date.year, date.month)[1]
+    date_end = (datetime.datetime(date.year, date.month,
+                                  1) + datetime.timedelta(
+        monthrange - 1)).strftime('%Y-%m-%d')
+    moudle_sql = moudle_sql.replace('__DAY1__', str(date_start)).replace(
+        '__DAY2__', str(date_end))
+    return moudle_sql
 
 
 if __name__ == "__main__":
@@ -135,12 +222,9 @@ if __name__ == "__main__":
 
     sparksession = (SparkSession
                     .builder
-                    .appName('select_insert_mysql_hive')
+                    .appName('pysparl_template')
                     .enableHiveSupport()
                     .getOrCreate())
-
-
-
 
     moudle_sql = """
      select 
@@ -171,26 +255,26 @@ if __name__ == "__main__":
 
     """
 
-
-
     # spark-submit 提供时间类型参数，计算日，周，月 day,week,month
-    time_type = sys.argv[1]
-
-    excute_date = str(sys.argv[2])
+    parser = argparse.ArgumentParser()
+    parser.add_argument('time_type')
+    parser.add_argument('excute_day')  # add_argument()指定程序可以接受的命令行选项
+    args = parser.parse_args()
+    time_type = args.time_type
+    excute_date = parse(str(args.excute_day))
 
     # 根据任务传入的时间类型，判断执行的任务是日任务，周任务或者月任务
     if time_type == 'day':
-        sql_str = day(date=excute_date, moudle_sql=moudle_sql)
+        sql_str = job_day(date=excute_date, moudle_sql=moudle_sql)
         print("sql_str:", sql_str)
         excute(sql_query=sql_str, sql_session=sparksession)
     elif time_type == 'week':
-        sql_str = week(date=excute_date, moudle_sql=moudle_sql)
+        sql_str = job_week(date=excute_date, moudle_sql=moudle_sql)
         print("sql_str:", sql_str)
         excute(sql_query=sql_str, sql_session=sparksession)
     else:
-        sql_str = month(date=excute_date, moudle_sql=moudle_sql)
+        sql_str = job_month(date=excute_date, moudle_sql=moudle_sql)
         print("sql_str:", sql_str)
         excute(sql_query=sql_str, sql_session=sparksession)
-
 
     sparksession.stop()
